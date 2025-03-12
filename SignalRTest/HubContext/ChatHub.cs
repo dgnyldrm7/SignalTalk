4ec113ffdb9c47f8c05c70388cff45a5eada9927
Dogan Yildirim <dgnyldrmcoc@gmail.com>
2025-03-13 00:57:46 +0300

    configured all methods

---
+++ SignalRTest/HubContext/ChatHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest.HubContext
+{
+    public class ChatHub : Hub
+    {
+        //Create a method to get connectionId
+        public string GetConnectionId()
+        {
+            return Context.ConnectionId;
+        }
+
+        //Kullanıcı diğer kullacıya mesaj atar!
+        public async Task SendMessageToUser(string connectionId, string message)
+        {
+            string senderId = Context.ConnectionId;
+
+            if (!string.IsNullOrEmpty(connectionId))
+            {
+                await Clients.Client(connectionId).SendAsync("SendToUser", senderId, message);
+
+                await Clients.Caller.SendAsync("SendToUser", "Ben", message);
+            }
+            else
+            {
+                await Clients.Caller.SendAsync("SendToUserBack", "Kullanıcı bulunamadı!");
+            }
+        }
+
+    }
+}
--- SignalRTest/Program.cs
+++ SignalRTest/Program.cs
@@ -1,3 +1,5 @@
+using SignalRTest.HubContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +9,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Add SignalR - SignalR Servicei burada ekledik.
+builder.Services.AddSignalR();
+
+//Add CORS - CORS Servicei burada ekledik.
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://127.0.0.1:5500", "http://localhost:5500") // Ýstemcinin URL’sini buraya ekle
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .AllowCredentials();
+    });
+});
+
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -15,6 +35,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Hub'ý belirli bir endpoint'e baðladýk. Keyfidir!
+app.MapHub<ChatHub>("/chatHub");
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
